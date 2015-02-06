33383959a98ec82fbded94fce0c988fb7d340271
Marcos Torres <marcost@lagash.com>
2015-02-06 12:04:57 -0800

    now the assembly are embedded resourcesand we extract the information from there

--- src/CLI/Vipr.CLI/SingleFileStrategy.cs
+++ src/CLI/Vipr.CLI/SingleFileStrategy.cs
@@ -1,9 +1,9 @@
 using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Linq;
-using System.Reflection;
+using System.Text;
 using Microsoft.VisualStudio.TextTemplating;
 using ODataReader.v4;
 using TemplateWriter;
@@ -44,7 +44,7 @@ namespace Vipr.CLI
 
             Model = _reader.GenerateOdcmModel(new Dictionary<string, string>
             {
-                { "$metadata", _arguments.BuilderArguments.InputFile }
+                { "$metadata", File.ReadAllText(_arguments.BuilderArguments.InputFile) }
             });
         }
 
@@ -52,39 +52,105 @@ namespace Vipr.CLI
 
         public OdcmModel Model { get; set; }
 
-        public void ComplexTypes(string path)
+        public void ComplexTypes(string templateFilePath)
         {
             var complexTypes = Model.GetComplexTypes();
+
             foreach (var complexType in complexTypes)
             {
                 var host = new CustomHost(Name, complexType) //TODO: v3? How?
                 {
-                    TemplateFile = "",
+                    TemplateFile = templateFilePath,
                 };
 
                 var templateContent = File.ReadAllText(host.TemplateFile);
                 var output = _engine.ProcessTemplate(templateContent, host);
+
+                if (host.Errors != null && host.Errors.HasErrors)
+                {
+                    var errors = LogErrors(host);
+                    throw new InvalidOperationException(errors);
+                }
                 _fileWriter.Write(output);
             }
         }
 
+        protected static string LogErrors(CustomHost host)
+        {
+            var sb = new StringBuilder();
+            if (host.Errors == null || host.Errors.Count <= 0) return sb.ToString();
+            foreach (CompilerError error in host.Errors)
+            {
+                sb.AppendLine("Error template" + host.TemplateFile);
+                sb.AppendLine(error.ErrorText);
+                sb.AppendLine("In line: " + error.Line);
+                sb.AppendLine(Environment.NewLine);
+            }
+            return sb.ToString();
+        }
+
         public void ProcessTemplates()
         {
-            var resources = typeof (CustomHost).Assembly.GetManifestResourceNames();
+            var resources = typeof(CustomHost).Assembly.GetManifestResourceNames();
             var templateNames = resources.Select(x =>
             {
                 var splits = x.Split('.');
-                return splits.ElementAt(splits.Count() - 2);
+                return new Tuple<string, string>(x, splits.ElementAt(splits.Count() - 2));
             });
 
+            var dict = new Dictionary<string, string>();
 
-            foreach (var template in templateNames)
+            foreach (var templateName in templateNames)
             {
-                if (Templates.Keys.Contains(template))
+                var path = GetTempFilePathWithExtension(templateName);
+                dict.Add(templateName.Item2, path);
+
+                Action<string> action;
+                if (Templates.TryGetValue(templateName.Item2, out action))
                 {
-                    ComplexTypes(template);
+                    string s;
+                    dict.TryGetValue(templateName.Item2, out s);
+                    action(s);
                 }
             }
+        }
+
+        private void CopyStream(Stream stream, string destPath)
+        {
+            using (var fileStream = new FileStream(destPath, FileMode.Create, FileAccess.Write))
+            {
+                stream.CopyTo(fileStream);
+            }
+        }
+
+        public string GetTempFilePathWithExtension(Tuple<string, string> templateName)
+        {
+            var fullpath = CreatePath(templateName.Item2 + ".tt");
+            CreateTempTemplateFile(templateName.Item1, fullpath);
+            return fullpath;
+        }
+
+        private void CreateTempTemplateFile(string resourceName, string fullpath)
+        {
+            if (!File.Exists(fullpath))
+            {
+                File.Delete(fullpath);
+            }
+
+            using (var stream = typeof(CustomHost).Assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream != null)
+                {
+                    CopyStream(stream, fullpath);
+                }
+            }
+        }
+
+        private static string CreatePath(string fileName)
+        {
+            var path = Path.GetTempPath();
+            var fullpath = Path.Combine(path, fileName);
+            return fullpath;
         }
 
         public string Name
