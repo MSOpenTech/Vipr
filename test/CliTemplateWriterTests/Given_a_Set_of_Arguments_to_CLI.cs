793986eaffd52d33b9000745bdb1e6505c72737e
Marcos Torres <marcost@lagash.com>
2015-02-25 05:05:07 -0800

    adding more config for the metadata file

--- test/CliTemplateWriterTests/Given_a_Set_of_Arguments_to_CLI.cs
+++ test/CliTemplateWriterTests/Given_a_Set_of_Arguments_to_CLI.cs
@@ -74,6 +74,26 @@ namespace CliTemplateWriterTests
                                                     .WithArguments(args);
             var entrypoint = new CLIEntryPoint(builder, new TemplateProcessorManager());
             entrypoint.Process();
+        }  
+        
+        [Fact]
+        public void When_passing_specific_Arguments_should_procces_discovery_metadata()
+        {
+            var args = "--language=java --inputFile=Metadata\\discovery.xml --outputDir=Out".Split(' ');
+            var builder = new ConfigurationBuilder().WithConfiguration(new DisoveryConfiguration())
+                                                    .WithArguments(args);
+            var entrypoint = new CLIEntryPoint(builder, new TemplateProcessorManager());
+            entrypoint.Process();
+        }       
+        
+        [Fact]
+        public void When_passing_specific_Arguments_should_procces_directory_metadata()
+        {
+            var args = "--language=java --inputFile=Metadata\\aad_graph_v15_augmented_v4.xml --outputDir=Out".Split(' ');
+            var builder = new ConfigurationBuilder().WithConfiguration(new DirectoryConfiguration())
+                                                    .WithArguments(args);
+            var entrypoint = new CLIEntryPoint(builder, new TemplateProcessorManager());
+            entrypoint.Process();
         }
 
         [Fact]
